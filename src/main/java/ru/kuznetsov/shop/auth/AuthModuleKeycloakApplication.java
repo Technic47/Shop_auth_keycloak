37964b8c96b3acb4d686a86b92ad08476d2c6a66
Pavel Kuznetsov <Technic47@gmail.com>
2025-11-02 13:45:37 +0300

    Init commit.

---
+++ src/main/java/ru/kuznetsov/shop/auth/AuthModuleKeycloakApplication.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthModuleKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthModuleKeycloakApplication.class, args);
+    }
+
+}
